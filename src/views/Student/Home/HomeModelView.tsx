357ebe700430a2e0c4ed3039ef13dc47861fe677
Christian <christiansoares336@gmail.com>
2025-05-28 20:53:39 -0300

    merge home student in progress

--- src/views/Student/Home/HomeModelView.tsx
+++ src/views/Student/Home/HomeModelView.tsx
@@ -1,16 +1,47 @@
 import Swal from "sweetalert2"
 
 import Aluno from "../../../models/AlunoModel"
+import Aula from "../../../models/AulaModel";
 import { BootstrapColors } from "../../../constants/Colors";
 
-export const get = async(
-    id: number
+// export const get = async(
+//     id: number
+// ) => {
+//         try{
+
+//         const response:any = await Aluno.get(
+//             id
+//         );
+
+//         if(response.success){
+//             return {
+//                 success: true,
+//                 data: response.data
+//             }
+//         }else{
+//             throw new Error(response.message||'Erro desconhecido.');
+//         }
+
+//     }catch(error:any){
+//         console.error(error);
+//         Swal.fire({
+//             icon: 'error',
+//             title: 'Oops',
+//             text: 'Erro ao tentar buscar os dados da sua conta.',
+//             confirmButtonColor: BootstrapColors.primary
+//         });
+//         return {
+//             success: false
+//         }
+//     }
+// }
+
+export const getAll = async(
+    id_aluno: number
 ) => {
         try{
 
-        const response:any = await Aluno.get(
-            id
-        );
+        const response:any = await Aula.getAll();
 
         if(response.success){
             return {
@@ -26,7 +57,7 @@ export const get = async(
         Swal.fire({
             icon: 'error',
             title: 'Oops',
-            text: 'Erro ao tentar buscar os dados da sua conta.',
+            text: 'Erro ao tentar buscar as aulas do aluno.',
             confirmButtonColor: BootstrapColors.primary
         });
         return {
--- src/views/Student/Home/HomeView.tsx
+++ src/views/Student/Home/HomeView.tsx
@@ -1,25 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
+import { faDoorOpen, faArrowLeftLong, faUserPlus, faPlus, faFloppyDisk, faTrash } from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
 
 import { ReactBootstrapComponents } from "../../../utils/Bootstrap";
 import { BootstrapColors } from "../../../constants/Colors";
 import { useGlobal } from "../../../context/GlobalContext";
 import Aluno from "../../../models/AlunoModel";
+import { getAll } from "./HomeModelView";
+import { ColorChoiser } from "../../../utils/ProjectLibs/colorChoiser";
+import { PieChart } from "../../../utils/ApexCharts";
 
 const HomeView = () => {
 
-    const [aluno,setAluno] = useState<Aluno|null>(null);
-    const [aulas, setAulas] = useState([
-        {
-            id: 1,
-            tx_descricao: "Aula 1"
-        }
-    ]);
+    const { state } = useGlobal();
+    const aluno:Aluno = state.user as Aluno;
+    const [aulas, setAulas] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const navigate = useNavigate();
     const { setUser } = useGlobal();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            const response = await getAll(aluno?.getId());
+
+            if(response?.success){
+                setAulas(response?.data);
+            }
+        }
+
+        fetchData().then(() => {
+            setLoading(false);
+        })
+    },[]);
 
      return(
         <ReactBootstrapComponents.Screen title="Painel do Aluno">
@@ -52,7 +67,82 @@ const HomeView = () => {
             />
             <br/><br/>
             <ReactBootstrapComponents.Accordion title="Meus Dados" isOpen={true}>
-                <div>OK</div>
+                <ReactBootstrapComponents.Alert 
+                    text={aluno?.getFeedback() as string} 
+                    variant={ColorChoiser.nivelVariant(aluno?.getTxNivel() as string)}
+                    style={{
+                        textAlign: 'center',
+                        padding: '2rem',
+                    }}
+                    />
+                    <h4 style={{marginTop: 20, marginBottom: 20}}>Acertos/Erros por tipo de Questão</h4>
+                    <div style={{display: 'flex', gap:15, flexWrap: 'wrap', justifyContent: 'center'}}>
+                        <ReactBootstrapComponents.Card>
+                            <PieChart
+                            title="Texto"
+                            data={[
+                                {
+                                label: 'Acertos',
+                                value: Number(aluno?.getNuAcertosTexto() || 0.0001)
+                                },
+                                {
+                                label: 'Erros',
+                                value: Number(aluno?.getNuErrosTexto() || 0.0001)
+                                }
+                            ]}
+                            width="100%"
+                            height={300}
+                            colors={[
+                                BootstrapColors.success,
+                                BootstrapColors.danger
+                            ]}
+                            />
+                        </ReactBootstrapComponents.Card>
+                        
+                        <ReactBootstrapComponents.Card>
+                            <PieChart
+                            title="Imagem"
+                            data={[
+                                {
+                                label: 'Acertos',
+                                value: Number(aluno?.getNuAcertosImagem() || 0.0001)
+                                },
+                                {
+                                label: 'Erros',
+                                value: Number(aluno?.getNuErrosImagem() || 0.0001)
+                                }
+                            ]}
+                            width="100%"
+                            height={300}
+                            colors={[
+                                BootstrapColors.success,
+                                BootstrapColors.danger
+                            ]}
+                            />
+                        </ReactBootstrapComponents.Card>
+                        
+                        <ReactBootstrapComponents.Card>
+                            <PieChart
+                            title="Vídeo"
+                            data={[
+                                {
+                                label: 'Acertos',
+                                value: Number(aluno?.getNuAcertosVideo() || 0.0001)
+                                },
+                                {
+                                label: 'Erros',
+                                value: Number(aluno?.getNuErrosVideo() || 0.0001)
+                                }
+                            ]}
+                            width="100%"
+                            height={300}
+                            colors={[
+                                BootstrapColors.success,
+                                BootstrapColors.danger
+                            ]}
+                            />
+                        </ReactBootstrapComponents.Card>
+                        </div>
             </ReactBootstrapComponents.Accordion>
             <br/>
             <ReactBootstrapComponents.Accordion title="Minhas Aulas" isOpen={true}>
